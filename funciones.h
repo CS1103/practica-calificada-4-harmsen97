b3a20f98b7c8f0c94fccae3615e66b9e70fe4c07
Jeronimo Harmsen <54420156+harmsen97@users.noreply.github.com>
2019-12-02 02:46:45 -0500

    Add files via upload

--- funciones.h
+++ funciones.h
@@ -10,6 +10,7 @@
 #include <iostream>
 #include <vector>
 #include <string_view>
+#include <cmath>
 #include "lodepng.h"
 using namespace std;
 
@@ -36,10 +37,10 @@ void Rfilter(string_view filename, std::vector<unsigned char>& image, unsigned w
 
     for (size_t i = 0; i < h; i++) {
         for (size_t j = 0; j < w * 4; j += 4) {
-             image[(i * w * 4 + j + 0)]; // Red component
+
              image[(i * w * 4 + j + 1)]=0; // Green component
              image[(i * w * 4 + j + 2)]=0; // Blue component
-             image[i * w * 4 + j + 3]; // Alpha component
+
         }
     }
 
@@ -58,8 +59,7 @@ void Bfilter(string_view filename, std::vector<unsigned char>& image, unsigned w
         for (size_t j = 0; j < w * 4; j += 4) {
             image[(i * w * 4 + j + 0)]=0; // Red component
             image[(i * w * 4 + j + 1)]=0; // Green component
-            image[(i * w * 4 + j + 2)]; // Blue component
-            image[i * w * 4 + j + 3]; // Alpha component
+
         }
     }
 
@@ -76,14 +76,67 @@ void Gfilter(string_view filename, std::vector<unsigned char>& image, unsigned w
     for (size_t i = 0; i < h; i++) {
         for (size_t j = 0; j < w * 4; j += 4) {
             image[(i * w * 4 + j + 0)]=0; // Red component
-            image[(i * w * 4 + j + 1)]; // Green component
+
             image[(i * w * 4 + j + 2)]=0; // Blue component
-            image[i * w * 4 + j + 3]; // Alpha component
+
         }
     }
 
 
     unsigned error = lodepng::encode(filename.data(), image, w, h);
+    if (error) {
+        std::cout << "encoder error " << error << ": " << lodepng_error_text(error) << std::endl;
+    }
+}
+
+void printConsole(string_view filename, std::vector<unsigned char>& image, unsigned w, unsigned h) {
+
+    for (size_t i = 0; i < h; i++) {
+        for (size_t j = 0; j < w * 4; j += 4) {
+            int r = image[i * w * 4 + j + 0]; // Red component
+            int g = image[i * w * 4 + j + 1]; // Green component
+            int b = image[i * w * 4 + j + 2]; // Blue component
+            int a = image[i * w * 4 + j + 3]; // Alpha component
+            std::cout << r << " ";
+            std::cout << g << " ";
+            std::cout << b << " ";
+            std::cout << a << "|";
+        }
+
+        std::cout <<endl;
+
+    }
+
+
+    unsigned error = lodepng::encode(filename.data(), image, w, h);
+    if (error) {
+        std::cout << "encoder error " << error << ": " << lodepng_error_text(error) << std::endl;
+    }
+}
+
+void Rotate45(string_view filename, std::vector<unsigned char> &image, unsigned w, unsigned h) {
+
+    auto aux = image;
+    int A = M_PI/4; // 45
+
+    for (size_t i = 0; i < h; i++) {
+        for (size_t j = 0; j < w * 4; j += 4) {
+             aux[i * w * 4 + j + 0]=image[((sin(A)*j + cos(A)*i)) * w * 4 + (cos(A)*j - sin(A)*i) + 0];
+             aux[i * w * 4 + j + 1]=image[((sin(A)*j + cos(A)*i)) * w * 4 + (cos(A)*j - sin(A)*i) + 1];
+             aux[i * w * 4 + j + 2]=image[((sin(A)*j + cos(A)*i)) * w * 4 + (cos(A)*j - sin(A)*i) + 2];
+             aux[i * w * 4 + j + 3]=image[((sin(A)*j + cos(A)*i)) * w * 4 + (cos(A)*j - sin(A)*i) + 3];
+               /*aux[i * w * 4 + j + 0]=image[(j-i) * w * 4 + (i+j) + 0];
+               aux[i * w * 4 + j + 1]=image[(j-i) * w * 4 + (i+j) + 1];
+               aux[i * w * 4 + j + 2]=image[(j-i) * w * 4 + (i+j) + 2];
+               aux[i * w * 4 + j + 3]=image[(j-i) * w * 4 + (i+j) + 3]; */
+
+        }
+
+
+
+    }
+
+    unsigned error = lodepng::encode(filename.data(), aux, w, h);
     if (error) {
         std::cout << "encoder error " << error << ": " << lodepng_error_text(error) << std::endl;
     }
--- main.cpp
+++ main.cpp
@@ -11,7 +11,9 @@ int main()
     //Bfilter("../blue.png", image, w, h);
 
     //Rfilter("../red.png", image, w, h);
-    Gfilter("../green.png", image, w, h);
+    //Gfilter("../green.png", image, w, h);
+    //printConsole("../out.png", image, w, h);
+    Rotate45("../rotate.png", image, w, h);
 
 
 }
